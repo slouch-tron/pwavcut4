8055bf636526b408a8c0b4e07878373cf93be69b
Adam <Adam@Slouch.com>
2024-03-16 11:15:09 -0700

    even more finicky debug output messing w/ with defaults.py. hopefully good practice for other cfgfile loading,   so we can count on being able to have a mostly empty extra cfgfile   with only nondefault values.

--- tools/defaults.py
+++ tools/defaults.py
@@ -11,35 +11,28 @@ DEBUG       = int(os.environ.get('DEBUG', 1))
 DBCOL       = "33;2"
 TRANSPARENT = int(os.environ.get('TRANSPARENT', 1))
 CFGFILE     = os.environ.get('CFGFILE', None)
-
-#DEFAULT_MIDI_LISTEN_CH      = 13
-#DEFAULT_MIDI_LISTEN_CH_OUT  = 13
-#DEFAULT_MIDI_LISTEN_CH_MOD  = 14
-#DEFAULT_MIDI_LISTEN_CH_KIT  = 15
+DIR_BASE    = os.environ.get('DIR_BASE', "/tmp")
 
 ## Paths
-DIR_BASE = "/tmp"
+#DIR_BASE = "/tmp"
 DEFAULT_WAV_IN_DIR  = os.path.join(DIR_BASE, 'wav_in')
 DEFAULT_WAV_OUT_DIR = os.path.join(DIR_BASE, 'wav_out')
 DEFAULT_SRC_OUT_DIR = os.path.join(DIR_BASE, 'source_out')
 DEFAULT_CONVERT_DIR = os.path.join(DIR_BASE, 'wav_convert')
 
 CFG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../cfg'))
-#LOG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../log'))
 DEFAULT_CFGFILE = os.path.join(CFG_PATH, "cfg.yml")
 
 ## What to run on/with
 OK_FILE_TYPES   = ['.wav', '.mp3', '.mp4', '.ogg']
 OK_ARCHS        = ['armv7l', 'x86_64', 'aarch64']   ## RPI4 uses 'aarch64', old one is 'armv7l'?
 
-
 ## Required
 assert(sys.version_info.major == 3)
 assert(os.path.isdir(DIR_BASE))
 assert(platform.machine() in OK_ARCHS)
 
-## SETUP base cfgfile and load globals from it
-##  some messy stuff but maybe we do want to run/check it every time
+## SETUP default cfg, exists to check against whether used
 ######################################################################
 _DEFAULT_MIDI_CHS = dict(
     OUT=15,
@@ -60,16 +53,6 @@ _DEFAULT_MIDI_CCS = dict(
     fxn3=33,
     )
 
-## if we really need to slot in a new DIR_BASE,
-## just do it here in this file
-_DEFAULT_DIRS = dict(
-    DIR_BASE="/tmp",
-    WAV_IN=os.path.join(DIR_BASE, 'wav_in'),
-    WAV_OUT=os.path.join(DIR_BASE, 'wav_out'),
-    SRC_OUT=os.path.join(DIR_BASE, 'source_out'),
-    WAV_CONVERT=os.path.join(DIR_BASE, 'wav_convert'),
-    )
-
 _DEFAULT_CFG = dict(
     CTRL=dict(
         CCS=_DEFAULT_MIDI_CCS,
@@ -77,26 +60,18 @@ _DEFAULT_CFG = dict(
         supported_filetypes=OK_FILE_TYPES,
         supported_archs=OK_ARCHS,
         ),
-    DIRS=_DEFAULT_DIRS,
     )
 
-## Stuff that happens up-front no matter what
-## No harm in asserting all the directories exist
-## Will we ever really benefit from making it portable?
-## Or is it better to bury all this deep inside the class?
-## Will we ever try to rapidly switch base dirs?
+## function needs to go up here if doing stuff in global scope
 ######################################################################
 def pr_debug(txt):
     if DEBUG:
         _file = os.path.splitext(os.path.split(__file__)[-1])[0]
         print(f"\033[%sm{_file} | {txt}\033[0m" %DBCOL, file=sys.stderr)
 
-def cfg2str(cfgdict):
-    return str(cfgdict).replace(' ', '').replace("'", '"')
-
 pr_debug(f"DEBUG={DEBUG}")
 
-## default directories, cfgfile
+## create default directories, cfgfile
 ######################################################################
 for DIR in [
     DEFAULT_WAV_IN_DIR, DEFAULT_WAV_OUT_DIR, 
@@ -127,34 +102,54 @@ else:
     raise FileNotFoundError
     sys.exit()
 
-
-## set globals from config.  previous defaults preserved as '_' attrs
+## set globals from config. note missing, same, or different for debug 
 ######################################################################
 _CTRL = _FILE_CFG['CTRL']
 if 'CHS' in _CTRL:
-    DEFAULT_MIDI_LISTEN_CH_OUT = _CTRL['CHS'].get('OUT', _DEFAULT_MIDI_CHS['OUT'])
-    DEFAULT_MIDI_LISTEN_CH_MOD = _CTRL['CHS'].get('MOD', _DEFAULT_MIDI_CHS['MOD'])
-    DEFAULT_MIDI_LISTEN_CH_KIT = _CTRL['CHS'].get('KIT', _DEFAULT_MIDI_CHS['KIT'])
+    _CHS = _CTRL['CHS']
+    DEFAULT_MIDI_LISTEN_CH_OUT = int(_CHS.get('OUT', _DEFAULT_MIDI_CHS['OUT']))
+    DEFAULT_MIDI_LISTEN_CH_MOD = int(_CHS.get('MOD', _DEFAULT_MIDI_CHS['MOD']))
+    DEFAULT_MIDI_LISTEN_CH_KIT = int(_CHS.get('KIT', _DEFAULT_MIDI_CHS['KIT']))
     DEFAULT_MIDI_LISTEN_CH = DEFAULT_MIDI_LISTEN_CH_OUT
+
+    for k, v in _DEFAULT_CFG['CTRL']['CHS'].items():
+        _v = _CHS.get(k, None)
+        _txt = f"CHS.{k:8} "
+
+        if _v == None:  _txt += f"{v:3} (default, not in cfgfile)"
+        elif _v != v:   _txt += f"{_v:3} (non-default, vs '{v}' from cfgfile)"
+        else:           _txt += f"{v:3}" ## pass
+
+        pr_debug(_txt)
+
 
 DEFAULT_MIDI_CCS = dict()
 if 'CCS' in _CTRL:
+    _CCS = _CTRL['CCS']
     for k, v in _DEFAULT_MIDI_CCS.items():
         DEFAULT_MIDI_CCS.update({ k : v })
-        if k in _CTRL['CCS']:
-            _val = _CTRL['CCS'][k]
+
+        _txt = f"CCS.{k:8} "
+        if k in _CCS:
+            _val = _CCS[k]
             if _val != v:     ## non-default
-                print(f"cfgfile: {k}={_val}, vs default={v}")
-                DEFAULT_MIDI_CCS.update({ k : _CTRL['CCS'][k] })
+                _txt += f"{_val:3} (non-default, vs default '{v}')"
+                DEFAULT_MIDI_CCS.update({ k : _CCS[k] })
+            else:
+                #continue
+                _txt += f"{v:3}"
+        else:
+            DEFAULT_MIDI_CCS.update({ k : v })
+            _txt += f"{v:3} (default, not in cfgfile)"
+
+        pr_debug(_txt)
+
 
 ## Best way to 'communicate' with the rest of the module..
 ## to import globals like DEFAULT_CHANNEL, or a dict like CFG_DICT.get('default_channel')
 ######################################################################
 LOADED_CFG = _FILE_CFG  
-pr_debug(cfg2str(LOADED_CFG))
 
-
-######################################################################
 ######################################################################
 def CURSE_INIT(stdscr):
     ## dont see these 2 in p_wavcut3
@@ -175,9 +170,12 @@ def CURSE_INIT(stdscr):
     return stdscr
 
 
+def cfg2str(cfgdict):
+    return str(cfgdict).replace(' ', '').replace("'", '"')
 
+######################################################################
 if __name__ == '__main__':
-    cfg = _FILE_CFG
+    cfg = LOADED_CFG
     ccc = cfg2str(cfg)
     print(ccc)
 
